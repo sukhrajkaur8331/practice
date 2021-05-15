f727e6219ead08e3758c9fd1cfb5534896d4517c
SukhrajKaur511 <Hp@SUKHRAJ>
2021-05-15 23:31:42 +0500

    first commit

---
+++ src/dd/Dog.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package dd;
+
+/**
+ * @author Hp
+ *
+ */
+public class Dog {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("do work");
+
+	}
+
+}
